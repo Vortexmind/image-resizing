cf2e0172d3cc3a51adbd4856e0e50c85f8f7ac06
Vortexmind <Vortexmind@users.noreply.github.com>
2020-08-26 20:16:35 +0100

    Lint & bugfix

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const ImageComponents = require('./src/imageComponents')
 
 addEventListener('fetch', event => {
-
   /* Get the origin image if the request is from the resizer worker itself */
-  if (/image-resizing/.test(event.request.headers.get("via"))) {
-    return fetch(event.request);
+  if (/image-resizing/.test(event.request.headers.get('via'))) {
+    return fetch(event.request)
   }
 
   event.respondWith(handleRequest(event.request))
@@ -14,29 +13,30 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  
-  let options = { cf: { 
-    image: {
-      quality : "85",
-      fit : "scale-down",
-      metadata : "copyright",
-      sharpen: 1.0
-    } } }
+  let options = {
+    cf: {
+      image: {
+        quality: '85',
+        fit: 'scale-down',
+        metadata: 'copyright',
+        sharpen: 1.0
+      }
+    }
+  }
 
   const imageUrl = new ImageComponents(event.request.url)
-  
-  if ( imageUrl.getSize() > 0 ) options.cf.image.width = imageUrl.getSize();
 
-  const imageRequest = new Request( imageUrl.getUnsizedUrl(), {
-    headers : request.headers
-  });
-  
+  if (imageUrl.getSize() > 0) options.cf.image.width = imageUrl.getSize()
+
+  const imageRequest = new Request(imageUrl.getUnsizedUrl(), {
+    headers: request.headers
+  })
+
   const response = await fetch(imageRequest, options)
 
   if (response.ok) {
-    return response;
+    return response
   } else {
-    return response.redirect(imageURL, 307);
+    return response.redirect(imageUrl.getInputUrl(), 307)
   }
-
 }
--- src/imageComponents.js
+++ src/imageComponents.js
@@ -1,33 +1,31 @@
-var kindOf = require('kind-of');
+var kindOf = require('kind-of')
 
 class ImageComponents {
+  constructor(url) {
+    const sizeMatch = new RegExp(/(.+)\/size\/w(\d+)(\/.+)/)
+    this.inputUrl = url
+    this.parts = url.match(sizeMatch)
+  }
 
-    constructor(url){
-        const sizeMatch = new RegExp(/(.+)\/size\/w(\d+)(\/.+)/)
-        this.inputUrl = url;
-        this.parts = url.match(sizeMatch);    
+  getSize() {
+    if (kindOf(this.parts) === 'array' && this.parts.length == 4) {
+      return parseInt(this.parts[2])
+    } else {
+      return -1
     }
+  }
 
-    getSize() {
-        if (kindOf(this.parts) === 'array' && this.parts.length == 4) { 
-            return parseInt(this.parts[2]);
-        } else {
-            return -1;
-        }
+  getUnsizedUrl() {
+    if (kindOf(this.parts) === 'array' && this.parts.length == 4) {
+      return this.parts[1].concat(this.parts[3])
+    } else {
+      return this.inputUrl
     }
+  }
 
-    getUnsizedUrl() {
-        if (kindOf(this.parts) === 'array' && this.parts.length == 4) { 
-            return this.parts[1].concat(this.parts[3]);
-        } else {
-            return this.inputUrl;
-        }
-    }
-
-    getInputUrl(){
-        return this.inputUrl;
-    }
-
+  getInputUrl() {
+    return this.inputUrl
+  }
 }
 
-module.exports = ImageComponents;
+module.exports = ImageComponents
