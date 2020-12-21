525c8ae699eb9f55995046ac9475d11045cb13b3
Paolo Tagliaferri <Vortexmind@users.noreply.github.com>
2020-12-21 16:31:28 +0000

    Remove avif test

--- test/resizerOptions.test.js
+++ test/resizerOptions.test.js
@@ -62,22 +62,6 @@ it('Selects webp format if available', () => {
     expect(resizerOptions.getOptions().cf.image.format).toBe('webp')
 });
 
-it('Selects avif format if available', () => {
-    let myHeaders = new Headers();
-    myHeaders.append('Content-Type', 'text/plain');
-    myHeaders.append('Accept', 'image/avif, */*');
-    myHeaders.append('Accept-Encoding','gzip, deflate, br')
-    myHeaders.append('Connection','keep-alive')
-    const resizerOptions = new ResizerOptions(myHeaders,1800)
-
-    expect(resizerOptions.getOptions().cf.image.quality).toBe('85')
-    expect(resizerOptions.getOptions().cf.image.fit).toBe('scale-down')
-    expect(resizerOptions.getOptions().cf.image.metadata).toBe('copyright')
-    expect(resizerOptions.getOptions().cf.image.sharpen).toBe(1.0)
-    expect(resizerOptions.getOptions().cf.image.width).toBe(1000)
-    expect(resizerOptions.getOptions().cf.image.format).toBe('avif')
-});
-
 it('Selects auto format if none other available', () => {
     let myHeaders = new Headers();
     myHeaders.append('Content-Type', 'text/plain');
@@ -95,6 +79,23 @@ it('Selects auto format if none other available', () => {
 });
 
 /*
+
+it('Selects avif format if available', () => {
+    let myHeaders = new Headers();
+    myHeaders.append('Content-Type', 'text/plain');
+    myHeaders.append('Accept', 'image/avif');
+    myHeaders.append('Accept-Encoding','gzip, deflate, br')
+    myHeaders.append('Connection','keep-alive')
+    const resizerOptions = new ResizerOptions(myHeaders,1800)
+
+    expect(resizerOptions.getOptions().cf.image.quality).toBe('85')
+    expect(resizerOptions.getOptions().cf.image.fit).toBe('scale-down')
+    expect(resizerOptions.getOptions().cf.image.metadata).toBe('copyright')
+    expect(resizerOptions.getOptions().cf.image.sharpen).toBe(1.0)
+    expect(resizerOptions.getOptions().cf.image.width).toBe(1000)
+    expect(resizerOptions.getOptions().cf.image.format).toBe('avif')
+});
+
 it('Prefers avif over webp if available', () => {
     let myHeaders = new Headers();
     myHeaders.append('Content-Type', 'text/plain');
