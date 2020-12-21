0db4367c074a38576e9c7bebc29c85c7c1b45f89
Paolo Tagliaferri <Vortexmind@users.noreply.github.com>
2020-12-21 16:02:19 +0000

    Add corner cases to resizer options test coverage

--- test/resizerOptions.test.js
+++ test/resizerOptions.test.js
@@ -13,6 +13,22 @@ it('Handles correctly default options', () => {
     expect(resizerOptions.getOptions().cf.image.metadata).toBe('copyright')
     expect(resizerOptions.getOptions().cf.image.sharpen).toBe(1.0)
     expect(resizerOptions.getOptions().cf.image.width).toBe(800)
+    expect(resizerOptions.getOptions().cf.image.format).toBe('webp')
+});
+
+it('Handles correctly default options, absence of Accept header and negative img size', () => {
+    let myHeaders = new Headers();
+    myHeaders.append('Content-Type', 'text/plain');
+    myHeaders.append('Accept-Encoding','gzip, deflate, br')
+    myHeaders.append('Connection','keep-alive')
+    const resizerOptions = new ResizerOptions(myHeaders,-1)
+
+    expect(resizerOptions.getOptions().cf.image.quality).toBe('85')
+    expect(resizerOptions.getOptions().cf.image.fit).toBe('scale-down')
+    expect(resizerOptions.getOptions().cf.image.metadata).toBe('copyright')
+    expect(resizerOptions.getOptions().cf.image.sharpen).toBe(1.0)
+    expect(resizerOptions.getOptions().cf.image.width).toBe(1000)
+    expect(resizerOptions.getOptions().cf.image.format).toBe('auto')
 });
 
 it('Caps the size at 1000px', () => {
@@ -110,16 +126,3 @@ it('Prefers avif over webp if available #2', () => {
     expect(resizerOptions.getOptions().cf.image.format).toBe('avif')
 });
 
-/*
-
-      this.options = {
-        cf: {
-          image: {
-            quality: '85',
-            fit: 'scale-down',
-            metadata: 'copyright',
-            sharpen: 1.0,
-          },
-        },
-      }
-*/
