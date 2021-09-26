c3baa24dfe7e610d855f07a84f02a1d13262eedc
Vortexmind <Vortexmind@users.noreply.github.com>
2021-09-26 19:07:01 +0100

    Include allow list of origins

--- test/imageComponents.test.js
+++ test/imageComponents.test.js
@@ -1,7 +1,7 @@
 const ImageComponents = require('../src/imageComponents')
 
 it('Manipulates correctly an absolute URL with size', () => {
-    const image = new ImageComponents("https://example.com/content/images/size/w300/2020/08/my-image.jpg")
+    const image = new ImageComponents("https://example.com/content/images/size/w300/2020/08/my-image.jpg",[])
     expect(image.getExtension()).toBe(".jpg");
     expect(image.getSize()).toBe(300)
     expect(image.getUnsizedUrl()).toBe("https://example.com/content/images/2020/08/my-image.jpg")
@@ -9,7 +9,7 @@ it('Manipulates correctly an absolute URL with size', () => {
 });
 
 it('Manipulates correctly an absolute URL without size', () => {
-    const image = new ImageComponents("https://example.com/content/images/2019/12/crazy-stuff.png")
+    const image = new ImageComponents("https://example.com/content/images/2019/12/crazy-stuff.png",[])
     expect(image.getExtension()).toBe(".png");
     expect(image.getSize()).toBe(-1)
     expect(image.getUnsizedUrl()).toBe("https://example.com/content/images/2019/12/crazy-stuff.png")
@@ -17,7 +17,7 @@ it('Manipulates correctly an absolute URL without size', () => {
 });
 
 it('Manipulates correctly a relative URL with size', () => {
-    const image = new ImageComponents("content/images/size/w800/2020/08/my-image.jpg")
+    const image = new ImageComponents("content/images/size/w800/2020/08/my-image.jpg",[])
     expect(image.getExtension()).toBe(".jpg");
     expect(image.getSize()).toBe(800)
     expect(image.getUnsizedUrl()).toBe("content/images/2020/08/my-image.jpg")
@@ -25,9 +25,27 @@ it('Manipulates correctly a relative URL with size', () => {
 });
 
 it('Manipulates correctly a relative URL without size', () => {
-    const image = new ImageComponents("content/images/2019/12/crazy-stuff.png")
+    const image = new ImageComponents("content/images/2019/12/crazy-stuff.png",[])
     expect(image.getExtension()).toBe(".png");
     expect(image.getSize()).toBe(-1)
     expect(image.getUnsizedUrl()).toBe("content/images/2019/12/crazy-stuff.png")
     expect(image.getInputUrl()).toBe("content/images/2019/12/crazy-stuff.png")
+});
+
+it('Allows an image from allowed origins list', () => {
+    const image = new ImageComponents("https://example.com/content/images/2020/08/my-image.jpg",["example.com","foo.org","bar.it"])
+    expect(image.getHostname()).toBe("example.com");
+    expect(image.isOriginAllowed()).toBe(true)
+});
+
+it('Disallows an image not part of allowed origins list', () => {
+    const image = new ImageComponents("https://www.notallowed.com/content/images/2020/08/my-image.jpg",["example.com","foo.org","bar.it"])
+    expect(image.getHostname()).toBe("www.notallowed.com");
+    expect(image.isOriginAllowed()).toBe(false)
+});
+
+it('Allows a relative image', () => {
+    const image = new ImageComponents("content/images/2019/12/crazy-stuff.png",["example.com","foo.org","bar.it"])
+    expect(image.getHostname()).toBe("");
+    expect(image.isOriginAllowed()).toBe(true)
 });
