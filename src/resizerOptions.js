ed8e6e782b00f524b4895da96bcbcaf672b247ee
Paolo Tagliaferri <Vortexmind@users.noreply.github.com>
2021-11-17 00:09:27 +0000

    Refactor resizer option func

--- src/resizerOptions.js
+++ src/resizerOptions.js
@@ -22,19 +22,22 @@ class ResizerOptions {
     if (this.size > 0) this.options.cf.image.width = this.size
     if (this.size > 1000 || this.size < 0) this.options.cf.image.width = 1000
 
-    if (acceptHeader.includes('image/avif')) {
-      this.options.cf.image.format = 'avif'
-    } else if (acceptHeader.includes('image/webp')) {
-      this.options.cf.image.format = 'webp'
-    } else {
-      this.options.cf.image.format = 'auto'
-    }
+    this.options.cf.image.format = getFormat(acceptHeader)
 
     if (this.extension === '.gif') {
       this.options.cf.image.format = 'auto'
     }
 
     return this.options
+  }
+
+  getFormat(acceptHeader) {
+    if (acceptHeader.includes('image/avif')) {
+      return 'avif'
+    } else if (acceptHeader.includes('image/webp')) {
+      return 'webp'
+    }
+      return 'auto'
   }
 }
 
